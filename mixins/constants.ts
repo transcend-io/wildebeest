e1a340131f74b441ba0c8a22a8bcb49ac170847c
michaelfarrell76 <mkfrl09@gmail.com>
2019-08-09 14:58:52 -0700

    Updates defaults to not use interface

--- mixins/constants.ts
+++ mixins/constants.ts
@@ -17,76 +17,76 @@ import { AssociationMixins } from './types';
 export const SEQUELIZE_MIXINS: AssociationMixins = {
   belongsToMany: ({ modelName, associationName }) => [
     {
-      attribute: `public readonly ${associationName.pluralCase}?: m.I${modelName.pascalCase}[];`,
+      attribute: `public readonly ${associationName.pluralCase}?: M.${modelName.plain}[];`,
     },
     {
-      attribute: `public get${associationName.pascalPluralCase}: w.BelongsToManyGetAssociationsMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public get${associationName.pascalPluralCase}: w.BelongsToManyGetAssociationsMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public add${associationName.pascalCase}: w.BelongsToManyAddAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public add${associationName.pascalCase}: w.BelongsToManyAddAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public add${associationName.pascalPluralCase}: w.BelongsToManyAddAssociationsMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public add${associationName.pascalPluralCase}: w.BelongsToManyAddAssociationsMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public has${associationName.pascalCase}: w.BelongsToManyHasAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public has${associationName.pascalCase}: w.BelongsToManyHasAssociationMixin<M.${modelName.plain}>;`,
     },
     {
       attribute: `public count${associationName.pascalPluralCase}: w.BelongsToManyCountAssociationsMixin;`,
     },
     {
-      attribute: `public create${associationName.pascalCase}: w.BelongsToManyCreateAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public create${associationName.pascalCase}: w.BelongsToManyCreateAssociationMixin<M.${modelName.plain}>;`,
     },
   ],
   belongsTo: ({ modelName, associationName }) => [
     {
-      attribute: `public ${associationName.plain}Id: m.I${modelName.pascalCase}['id'];`,
+      attribute: `public ${associationName.plain}Id: M.${modelName.plain}['id'];`,
     },
     {
-      attribute: `public readonly ${associationName.plain}?: m.I${modelName.pascalCase};`,
+      attribute: `public readonly ${associationName.plain}?: M.${modelName.plain};`,
     },
     {
-      attribute: `public get${associationName.pascalCase}: w.BelongsToGetAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public get${associationName.pascalCase}: w.BelongsToGetAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public set${associationName.pascalCase}: w.BelongsToSetAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public set${associationName.pascalCase}: w.BelongsToSetAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public create${associationName.pascalCase}: w.BelongsToCreateAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public create${associationName.pascalCase}: w.BelongsToCreateAssociationMixin<M.${modelName.plain}>;`,
     },
   ],
   hasMany: ({ modelName, associationName }) => [
     {
-      attribute: `public readonly ${associationName.pluralCase}?: m.I${modelName.pascalCase}[];`,
+      attribute: `public readonly ${associationName.pluralCase}?: M.${modelName.plain}[];`,
     },
     {
-      attribute: `public get${associationName.pascalPluralCase}: w.HasManyGetAssociationsMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public get${associationName.pascalPluralCase}: w.HasManyGetAssociationsMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public add${associationName.pascalCase}: w.HasManyAddAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public add${associationName.pascalCase}: w.HasManyAddAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public has${associationName.pascalCase}: w.HasManyHasAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public has${associationName.pascalCase}: w.HasManyHasAssociationMixin<M.${modelName.plain}>;`,
     },
     {
       attribute: `public count${associationName.pascalPluralCase}: w.HasManyCountAssociationsMixin;`,
     },
     {
-      attribute: `public create${associationName.pascalCase}: w.HasManyCreateAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public create${associationName.pascalCase}: w.HasManyCreateAssociationMixin<M.${modelName.plain}>;`,
     },
   ],
   hasOne: ({ modelName, associationName }) => [
     {
-      attribute: `public readonly ${associationName.plain}?: m.I${modelName.pascalCase};`,
+      attribute: `public readonly ${associationName.plain}?: M.${modelName.plain};`,
     },
     {
-      attribute: `public get${associationName.pascalCase}: w.HasOneGetAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public get${associationName.pascalCase}: w.HasOneGetAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public set${associationName.pascalCase}: w.HasOneSetAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public set${associationName.pascalCase}: w.HasOneSetAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public create${associationName.pascalCase}: w.HasOneCreateAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public create${associationName.pascalCase}: w.HasOneCreateAssociationMixin<M.${modelName.plain}>;`,
     },
   ],
 };
@@ -98,18 +98,18 @@ export const WILDEBEEST_MIXINS: AssociationMixins = {
   belongsToMany: () => [],
   belongsTo: ({ modelName, associationName }) => [
     {
-      attribute: `public getCached${associationName.pascalCase}: w.BelongsToGetCachedAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public getCached${associationName.pascalCase}: w.BelongsToGetCachedAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public get${associationName.pascalCase}OrDefault: w.BelongsToGetOrDefaultAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public get${associationName.pascalCase}OrDefault: w.BelongsToGetOrDefaultAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public updateOrCreate${associationName.pascalCase}: w.BelongsToUpdateOrCreateAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public updateOrCreate${associationName.pascalCase}: w.BelongsToUpdateOrCreateAssociationMixin<M.${modelName.plain}>;`,
     },
   ],
   hasMany: ({ modelName, associationName }) => [
     {
-      attribute: `public createMany${associationName.pascalPluralCase}: w.HasManyCreateManyAssociationsMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public createMany${associationName.pascalPluralCase}: w.HasManyCreateManyAssociationsMixin<M.${modelName.plain}>;`,
     },
     {
       attribute: `public destroyAll${associationName.pascalPluralCase}: w.HasManyDestroyAllAssociationMixin;`,
@@ -118,27 +118,27 @@ export const WILDEBEEST_MIXINS: AssociationMixins = {
       attribute: `public destroyOne${associationName.pascalCase}: w.HasManyDestroyOneAssociationMixin;`,
     },
     {
-      attribute: `public getOne${associationName.pascalCase}: w.HasManyGetOneAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public getOne${associationName.pascalCase}: w.HasManyGetOneAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public updateAll${associationName.pascalPluralCase}: w.HasManyUpdateAllAssociationsMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public updateAll${associationName.pascalPluralCase}: w.HasManyUpdateAllAssociationsMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public updateOne${associationName.pascalCase}: w.HasManyUpdateOneAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public updateOne${associationName.pascalCase}: w.HasManyUpdateOneAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public updateOrCreate${associationName.pascalCase}: w.HasManyUpdateOrCreateAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public updateOrCreate${associationName.pascalCase}: w.HasManyUpdateOrCreateAssociationMixin<M.${modelName.plain}>;`,
     },
   ],
   hasOne: ({ modelName, associationName }) => [
     {
-      attribute: `public getCached${associationName.pascalCase}: w.HasOneGetCachedAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public getCached${associationName.pascalCase}: w.HasOneGetCachedAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public get${associationName.pascalCase}OrDefault: w.HasOneGetOrDefaultAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public get${associationName.pascalCase}OrDefault: w.HasOneGetOrDefaultAssociationMixin<M.${modelName.plain}>;`,
     },
     {
-      attribute: `public updateOrCreate${associationName.pascalCase}: w.HasOneUpdateOrCreateAssociationMixin<m.I${modelName.pascalCase}>;`,
+      attribute: `public updateOrCreate${associationName.pascalCase}: w.HasOneUpdateOrCreateAssociationMixin<M.${modelName.plain}>;`,
     },
   ],
 };
