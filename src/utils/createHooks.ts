907c33376529c900439618365a92b27b1ead18b0
Michael Farrell <mkfrl09@gmail.com>
2020-04-07 19:36:14 -0700

    Adds transaction to before/after validate and fixes createHooks (#65)
    Co-authored-by: michaelfarrell76 <mike@transcend.io>

--- src/utils/createHooks.ts
+++ src/utils/createHooks.ts
@@ -1,9 +1,6 @@
-// external
-import { ModelHooks } from 'sequelize/types/lib/hooks';
-
 // global
 import { WildebeestModel } from '@wildebeest/classes';
-import { ModelMap } from '@wildebeest/types';
+import { HookOptions, ModelMap } from '@wildebeest/types';
 
 /**
  * Create database model hooks for a db model
@@ -12,7 +9,7 @@ import { ModelMap } from '@wildebeest/types';
  * @returns The type-enforced hooks
  */
 export default function createHooks<TModel extends WildebeestModel<ModelMap>>(
-  hooks: Partial<ModelHooks<TModel>>,
-): Partial<ModelHooks<TModel>> {
+  hooks: Partial<HookOptions<TModel>>,
+): Partial<HookOptions<TModel>> {
   return hooks;
 }
